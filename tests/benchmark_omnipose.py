6af615f169ca72a5d431b76e3ea72af6bcbf65b6
kmpape <kempf.idris@gmail.com>
2023-03-29 17:23:54 +0100

    Added iterations over rows for benchmarking

--- tests/benchmark_omnipose.py
+++ tests/benchmark_omnipose.py
@@ -1,18 +1,25 @@
-import copy
 from copy import deepcopy
 from glob import glob
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 from natsort import natsorted
 import numpy as np
+import os
+import psutil
 from skimage.transform import rescale, resize, downscale_local_mean
 from skimage.util import img_as_ubyte
+import sys
 import tifffile
 from time import perf_counter
 
 from cellpose import models, core, plot, io, transforms
 import omnipose
 from omnipose.utils import normalize99
+
+os_used = sys.platform
+process = psutil.Process(os.getpid())  # Set highest priority for the python script for the CPU
+if os_used == "win32":  # Windows (either 32-bit or 64-bit)
+    process.nice(psutil.REALTIME_PRIORITY_CLASS)
+elif os_used == "linux":  # linux
+    process.nice(psutil.IOPRIO_HIGH)
 
 save_dir = "/home/hslab/workspace_python/symbac_pip/testdata/"
 model_dir = save_dir + "train/models/"
@@ -54,27 +61,24 @@ nruns = 10
 model_loaded = True
 loop_run = True
 
-omni = True
-for batch_size in range(8, 14):  # number of 224 x 224 batches they send to the GPU
-    elapsed = 0
+omni_vec = [True]
+batch_size_vec = [8]
+irow_vec = range(0, num_tiles[0])
+
+assert nruns > 2
+omni = omni_vec[0]
+batch_size = batch_size_vec[0]
+#for omni in omni_vec:
+#    for batch_size in batch_size_vec:  # number of 224 x 224 batches they send to the GPU
+for irow in irow_vec:
+    elapsed = np.zeros(nruns)
+    x = [large_img[irow * img_shape[0]:(irow + 1) * img_shape[0], :]]
     for irun in range(0, nruns):
         start = perf_counter()
         masks, flows, styles = model.eval(
-            x=[large_img], batch_size=batch_size, channels=[0, 0], rescale=None, mask_threshold=-1,
-            transparency=True, flow_threshold=0., omni=omni, resample=True, verbose=0,
-            model_loaded=model_loaded, loop_run=loop_run)
-        elapsed += perf_counter() - start
-    print("Omni={:1}\tBatch Size={:2}\tElapsed={:.4f}".format(omni, batch_size, elapsed/nruns))
-
-omni = False
-for batch_size in range(8, 14):
-    elapsed = 0
-    for irun in range(0, nruns):
-        start = perf_counter()
-        masks, flows, styles = model.eval(
-            x=[large_img], batch_size=batch_size, channels=[0, 0], rescale=None, mask_threshold=-1,
-            transparency=True, flow_threshold=0., omni=omni, resample=True, verbose=0,
-            model_loaded=model_loaded, loop_run=loop_run)
-        elapsed += perf_counter() - start
-    print("Omni={:1}\tBatch Size={:2}\tElapsed={:.4f}".format(omni, batch_size, elapsed/nruns))
-
+            x=x, batch_size=batch_size, channels=[0, 0], rescale=None, mask_threshold=-1,
+            transparency=True, flow_threshold=0., omni=omni, resample=True, verbose=0)
+        elapsed[irun] = perf_counter() - start
+    avg_elapsed = (elapsed.sum()-elapsed.min()-elapsed.max())/(nruns-2.)
+    print("Omni={:1}\tBatch Size={:2}\tnrows={:2}\tElapsed_s={:.4f}\tElapsedPerTrench_ms={:.4f}\t".format(
+        omni, batch_size, irow+1, avg_elapsed, avg_elapsed/(irow+1)/num_tiles[1]*1000))
